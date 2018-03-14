c00b5c3531b94d2695c0709bfb6463961c729314
osamtimizer <eaten.jb@gmail.com>
2018-03-14 17:23:34 +0900

    Implement Social Login and Timer

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import firebase from 'firebase'
 
-var config = {
+let config = {
   apiKey: "AIzaSyDUBdU1s_1ff_yUxXvlCbS9y4JyocdaShk",
   authDomain: "simplestpomodoro.firebaseapp.com",
   databaseURL: "https://simplestpomodoro.firebaseio.com",
@@ -10,15 +10,26 @@ var config = {
 firebase.initializeApp(config);
 
 const auth = firebase.auth();
+const database = firebase.database();
 
 $(() => {
   auth.onAuthStateChanged((user) => {
     $("p#error_message").text();
     if (user) {
       //user is logged in
-      const displayName= user.displayName;
-      const displayText= "logged in as: " + displayName;
-      $("#username").text(displayText);
+      const uid = user.uid;
+      console.log("uid: ", uid);
+      let _displayName = "";
+      const ref = database.ref('users/' + uid + '/profile');
+      ref.once('value').then((snapshot) => {
+        const displayName = snapshot.child('displayName').val();
+        console.log("DisplayName: ", displayName);
+        const displayText= "logged in as: " + displayName;
+        $("#username").text(displayText);
+      }).catch((err) => {
+        console.error("Error: ", err);
+        return;
+      });
 
       fetch("pomodoro.html").then((res) => {
         console.log("Fetch is called");
@@ -36,23 +47,47 @@ $(() => {
       //user isn't logged in
       $("div.body").html('<button class="auth">auth</button>');
       initializeComponent();
-      $("#username").text();
+      $("#username").text("");
       $("button#logout").hide();
     }
   });
 
-  initializeComponent();
 });
 
 let initializeComponent = () => {
   $("button.auth").click((event) => {
     //authentication settings
     var provider = new firebase.auth.GoogleAuthProvider();
-    console.log(provider);
     firebase.auth().signInWithPopup(provider).then((result) => {
+      console.log(result);
+      console.log("success login");
       var token = result.credential.accessToken;
       var user = result.user;
-
+      var ref = database.ref('users');
+      //usersの中にuidと一致するものがなかったら新規追加する
+      ref.once('value').then((snapshot) => {
+        if (snapshot.hasChild(user.uid)) {
+          //nothing to do
+        } else {
+          console.log("New user");
+          const userRef = ref.child(user.uid);
+          userRef.set({
+            profile: {
+              displayName: user.displayName,
+              accessToken: token,
+              expire: 24
+            },
+            pomodoro: {
+              remain: 60 * 25 * 1000,
+              terms: 4,
+              isWorking: true
+            }
+          });
+        }
+        return true;
+      }).catch((err) => {
+        console.error("Error: ", err);
+      });
     }).catch((err) => {
       console.error("Error: Authentication failed with ", err.message);
       $("p#error_message").text(err.message);
--- public/javascripts/pomodoro.js
+++ public/javascripts/pomodoro.js
@@ -1,10 +1,50 @@
 import $ from 'jquery';
+import firebase from 'firebase';
 
-var remain = 60 * 25 * 1000;
-var interval = 60 * 5 * 1000;
-var timer;
+let config = {
+  apiKey: "AIzaSyDUBdU1s_1ff_yUxXvlCbS9y4JyocdaShk",
+  authDomain: "simplestpomodoro.firebaseapp.com",
+  databaseURL: "https://simplestpomodoro.firebaseio.com",
+  storageBucket: "simplestpomodoro.appspot.com",
+};
+firebase.initializeApp(config);
+
+const auth = firebase.auth();
+const database= firebase.database();
+
+const BREAK_TERM = false;
+const WORKING_TERM = true;
+const INITIAL_TERM_COUNT = 4;
+const MIN_MS = 60 * 1000;
+const WORKING_DURATION_MIN = 25;
+const BREAK_SMALL_DURATION_MIN = 5;
+const BREAK_LARGE_DURATION_MIN = 30;
+const ONE_SEC_MS = 1000;
+
+let isWorking = true;
+let terms = INITIAL_TERM_COUNT;
+let remain = WORKING_DURATION_MIN * MIN_MS;
+let timer;
 
 $(() => {
+  auth.onAuthStateChanged((user) => {
+    console.log("onAuthStateChanged is called");
+    if (user) {
+      console.log("user is logged in");
+      const pomodoroRef = database.ref('users/' + user.uid +'/pomodoro/');
+      pomodoroRef.once('value').then((snapshot) => {
+        remain = snapshot.child('remain').val();
+        terms = snapshot.child('terms').val();
+        isWorking = snapshot.child('isWorking').val();
+        refreshTimer();
+      }).catch((err) => {
+        console.error("Error: ", err);
+      });
+    } else {
+      //nothing to do
+    }
+  });
+
   $("button.start").click((event) => {
     timer = setInterval(startCount, 1000);
     $("button.start").prop("disabled", true);
@@ -13,10 +53,62 @@ $(() => {
   $("button.stop").click((event) => {
     $("button.start").prop("disabled", false);
     clearInterval(timer);
+    refreshDB();
   });
 });
 
 const startCount = () => {
-  remain = remain - 1000;
-  $("p.time").text(new Date(remain).toISOString().slice(14, -5));
+  if (remain <= 0) {
+    //Toggle Working<->Break
+    isWorking = !isWorking;
+    if (isWorking) {
+      remain = WORKING_DURATION_MIN * MIN_MS;
+      refreshTimer();
+      refreshDB();
+      return;
+
+    } else {
+      if (terms === 0) {
+        //Have a long break
+        remain = BREAK_LARGE_DURATION_MIN * MIN_MS;
+        terms = INITIAL_TERM_COUNT;
+      } else {
+        remain = BREAK_SMALL_DURATION_MIN * MIN_MS;
+        terms -= 1;
+      }
+
+      refreshTimer();
+      refreshDB();
+      return;
+    }
+  }
+
+  remain -= ONE_SEC_MS * 100;
+  refreshTimer();
 };
+
+const refreshDB = () => {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      const pomodoroRef = database.ref('users/' + user.uid +'/pomodoro/');
+      pomodoroRef.set({
+        remain: remain,
+        terms: terms,
+        isWorking: isWorking
+      });
+    } else {
+      //nothing todo
+    }
+  });
+}
+
+const refreshTimer = () => {
+  $("p.time").text(new Date(remain).toISOString().slice(14, -5));
+  $("p.term").text("Terms" + terms.toString());
+  if (isWorking) {
+    $("p.currentStatus").text("Status: Working");
+  } else {
+    $("p.currentStatus").text("Status: Break");
+  }
+
+}
